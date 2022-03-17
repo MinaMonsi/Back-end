55778108e1f7c8747f75349e6b3c0348c493b03e
Marina Monsivais <minamonsi@me.com>
2022-03-17 10:25:07 -0500

    server running

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) =>{
-    res.render('home')
+    // renders the home.ejs template
+    res.render('home') 
 })
 
 app.listen(3001, ()=> {
